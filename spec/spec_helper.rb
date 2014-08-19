5b4fe49fad1fd01caab11686808376cc8374dd7d
Catherine Tran & Kyle Smith <student@epicodus.com>
2014-08-19 10:04:48 -0700

    add rspec configuration loop to delete data that is created so that specs don't interfere with each.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+require 'pg'
+require 'line'
+require 'station'
+require 'pry'
+
+
+DB = PG.connect ({:dbname => "test_train_system"})
+
+RSpec.configure do |config|
+  config.before(:each) do
+    DB.exec("DELETE FROM lines *")
+    DB.exec("DELETE FROM stations *")
+    # DB.exec("DELETE FROM stops *")
+  end
+end
