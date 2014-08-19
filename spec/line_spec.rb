f89045967f00481312e3d85bd30a01b17d8eb7f9
Catherine Tran & Kyle Smith <student@epicodus.com>
2014-08-19 10:38:38 -0700

    fix typo in first spec.

--- spec/line_spec.rb
+++ spec/line_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 
 describe Line do
-  it 'initializes an line with a name' do
+  it 'initializes a line with a name' do
     test_line = Line.new({:name => "Red"})
     expect(test_line).to be_an_instance_of Line
   end
