c1b72c41b1d12d1f21ce05ea8d3e46adfc3f1026
Catherine Tran & Kyle Smith <student@epicodus.com>
2014-08-19 11:49:53 -0700

    intializes station with a name

---
+++ spec/station_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+
+describe Station do
+
+  it 'initializes a station with a name' do
+    test_station = Station.new({:name => "A"})
+    expect(test_station).to be_an_instance_of Station
+  end
+
+
+end
